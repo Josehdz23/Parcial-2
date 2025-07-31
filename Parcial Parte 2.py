e229ba0f6a3c7b1eceda824cc4452b0ab53e4830
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-30 18:47:48 -0600

    Se hizo la primera función del MCD entre dos números, se empieza con la cadena repetida N veces

--- Parcial Parte 2.py
+++ Parcial Parte 2.py
@@ -1,6 +1,29 @@
 def menu():
     print("\n - - - - MENÚ - - - -\n1. Calcular el MCD de dos números\n2. Cadena repetida N veces\n3. Contar cuantas veces aparece una letra\n4. Convertir un número decimal a binario\n5. Calcular cuantos digitos tiene un número\n6. Salir")
 
+def calcularMCD():
+    while True:
+        try:
+            n1 = int(input("Ingrese el primer número: "))
+            if n1 > 0:
+                n2 = int(input("Ingrese el segundo número: "))
+                if n2 > 0:
+                    calculoMCD(n1, n2)
+                    print(f"El MCD de {n1} y {n2} es: {calculoMCD(n1, n2)}")
+            else:
+                print("El 0 no está permitido ")
+        except Exception as ex:
+            print(f"Ha ocurrido un error: {ex}")
+def calculoMCD(n1, n2):
+    if n2 == 0:
+        return n1
+    else:
+        return  calculoMCD(n2, n1 % n2)
+
+def cadenaRepetida():
+    while True:
+        cadena = input("Ingrese una palabra: ")
+
 def main():
     while True:
         try:
@@ -8,9 +31,9 @@ def main():
             op = int(input("Seleccione una opción: "))
             match op:
                 case 1:
-                    print("Opción 1")
+                    calcularMCD()
                 case 2:
-                    print("Opción 2")
+                    cadenaRepetida()
                 case 3:
                     print("Opción 3")
                 case 4:
